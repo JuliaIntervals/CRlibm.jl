70fa408291dfe98085073b3c30c53fe4762c6ac6
David Sanders <dpsanders@gmail.com>
2015-09-18 14:45:56 -0500

    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,12 @@ using CRlibm
 using Base.Test
 
 @test cos(0.5, RoundDown) == 0.8775825618903726
-@test cos(0.5, RoundUp) = 0.8775825618903728
-@test cos(0.5, RoundNearest) == 0.8775825618903728
-@test cos(0.5, RoundNearest) == cos(0.5)
+@test cos(0.5, RoundUp) == 0.8775825618903728
+@test cos(0.5, RoundNearest) == cos(0.5) == 0.8775825618903728
 
-@test cos(0.5, RoundUp) - cos(0.5, RoundDown) == eps(cos(0.5, RoundDown))
+for f in (sin, cos, tan, log, exp)
+    for val in (0.5, 103.2, -17.1)
+        val < 0.0 && f==log && continue
+        @test f(val, RoundUp) - f(val, RoundDown) == eps(f(val, RoundDown))
+    end
+end

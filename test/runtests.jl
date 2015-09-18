5a41beb18e67ef4a223cae5e9bdb23714d806a33
David Sanders <dpsanders@gmail.com>
2015-09-18 14:40:02 -0500

    Fix a test

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@ using Base.Test
 
 @test cos(0.5, RoundDown) == 0.8775825618903726
 @test cos(0.5, RoundUp) = 0.8775825618903728
-@test cos(0.5, RoundNearest) == cos(0.5) == 0.8775825618903728
+@test cos(0.5, RoundNearest) == 0.8775825618903728
+@test cos(0.5, RoundNearest) == cos(0.5)
 
 @test cos(0.5, RoundUp) - cos(0.5, RoundDown) == eps(cos(0.5, RoundDown))
